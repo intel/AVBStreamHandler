0aa2acc6489fab1d925bc1ab513f1a1250b48c47
Ederson de Souza <ederson.desouza@intel.com>
2018-10-02 13:48:09 -0700

    A gstreamer plugin sample: avbvideosink element
    To aid visualisation of the videos being sent through AVB-SH, one can
    use a GStreamer interface, so that packets can follow a GStreamer
    pipeline.
    
    This patch adds a gstreamer sink element, that can get MPEG-TS packets
    and send them to the AVB-SH AvbVideoBridge - which gets the packets and
    send them down AVB-SH stack.
    
    This element, `avbvideosink`, has a property "stream-name", associated
    to the "IPC name" property set on AVB-SH configuration. That way, the
    element can connect to a specific AVB-SH stream.
    
    An example of using this element could be the following gstreamer
    pipeline, on the talker:
    
    $ LD_LIBRARY_PATH=$AVB_DEPS/lib/:. GST_PLUGIN_PATH=./build gst-launch-1.0 videotestsrc pattern=ball ! clockoverlay ! vaapih264enc ! mpegtsmux ! avbvideosink
    
    This will send the video produced by `videotestsrc` (with the clock
    overlay added by `clockoverlay`) to the h264 encoder, the MPEG-TS muxer
    and finally to the avbvideosink, which will send it via network using
    AVB-SH stack.

---
+++ private/inc/gst/gstavbvideosink.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _GST_AVBVIDEOSINK_H_
+#define _GST_AVBVIDEOSINK_H_
+
+#include <gst/base/gstbasesink.h>
+
+#include "media_transport/avb_video_bridge/IasAvbVideoBridge.h"
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_AVBVIDEOSINK   (gst_avbvideosink_get_type())
+#define GST_AVBVIDEOSINK(obj)   (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_AVBVIDEOSINK,GstAvbVideoSink))
+#define GST_AVBVIDEOSINK_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_AVBVIDEOSINK,GstAvbVideoSinkClass))
+#define GST_IS_AVBVIDEOSINK(obj)   (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_AVBVIDEOSINK))
+#define GST_IS_AVBVIDEOSINK_CLASS(obj)   (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_AVBVIDEOSINK))
+
+typedef struct _GstAvbVideoSink GstAvbVideoSink;
+typedef struct _GstAvbVideoSinkClass GstAvbVideoSinkClass;
+
+struct _GstAvbVideoSink
+{
+    GstBaseSink base_avbvideosink;
+
+    struct ias_avbvideobridge_sender *sender;
+    gchar *stream_name;
+};
+
+struct _GstAvbVideoSinkClass
+{
+    GstBaseSinkClass base_avbvideosink_class;
+};
+
+GType gst_avbvideosink_get_type(void);
+
+G_END_DECLS
+
+#endif
