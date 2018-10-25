54be387bafbc79cac84599a8f2ac74883fd911d5
Keerock Lee <keerock.lee@intel.com>
2018-10-25 00:50:54 +0000

    Enable avb_streamhandler_demo in make install
    1. add below lib and exe for make install
    
    /usr/lib64/libias-media_transport-avb_streamhandler.so
    /usr/lib64/pluginias-media_transport-avb_configuration_reference.so
    /usr/bin/avb_streamhandler_demo
    2. setcap to /usr/bin/avb_streamhandler_demo
    3. modify setcap.sh

--- setcap.sh
+++ setcap.sh
@@ -11,14 +11,19 @@
 if [ -x "$AVB_SETCAP_TOOL" ]; then
 	echo "Using setcap_tool defined on AVB_SETCAP_TOOL env var"
 	$AVB_SETCAP_TOOL $1 && exit
-	echo "Failed"
+	echo "Failed to set cap"
 fi
-
 which setcap_tool &> /dev/null
 if [ $? -eq 0 ]; then
 	echo "Trying to use setcap_tool on PATH env var"
 	setcap_tool $1 && exit
-	echo "Failed"
+	echo "Failed to set cap"
+fi
+which setcap &> /dev/null
+if [ $? -eq 0 ]; then
+	echo "running setcap on PATH env var"
+	setcap cap_net_admin,cap_net_raw,cap_sys_nice+ep $1 && exit
+	echo "Failed to set cap"
 fi
 
 echo "Warning: echo setcap_tool not installed"
